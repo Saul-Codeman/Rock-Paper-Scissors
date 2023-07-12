6dc131f12a410e0c4e19abaa325bf0689cffa044
Sean Roberts <seanarmandroberts@outlook.com>
2023-07-12 15:31:25 -0400

    Initial commit for UI

--- javascript.js
+++ javascript.js
@@ -1,22 +1,17 @@
 function getComputerChoice(){
     const randomInteger = Math.floor(Math.random() * 3);
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = ["rock", "paper", "scissors"];
     return choices[randomInteger]
-}
-
-function capitalize(string){
-    string = string[0].toUpperCase() + string.slice(1);
-    return string
 }
 
 function playRound(computerChoice, userChoice, playerScore, computerScore){
     let result = "";
     if (userChoice === computerChoice){
         result = `You tied. ${userChoice} equals ${computerChoice}.`
-    } else if (((userChoice === "Rock") && (computerChoice === "Scissors")) || ((userChoice === "Scissors") && (computerChoice == "Paper")) || ((userChoice === "Paper") && (computerChoice == "Rock"))) {
+    } else if (((userChoice === "rock") && (computerChoice === "scissors")) || ((userChoice === "scissors") && (computerChoice == "paper")) || ((userChoice === "paper") && (computerChoice == "rock"))) {
         playerScore+=1;
         result = `You win! ${userChoice} beats ${computerChoice}.`
-    }else if (((userChoice === "Rock") && (computerChoice === "Paper")) || ((userChoice === "Scissors") && (computerChoice == "Rock")) || ((userChoice === "Paper") && (computerChoice == "Scissors"))) {
+    }else if (((userChoice === "rock") && (computerChoice === "paper")) || ((userChoice === "scissors") && (computerChoice == "rock")) || ((userChoice === "paper") && (computerChoice == "scissors"))) {
         computerScore+=1;
         result = `You lost. ${computerChoice} beats ${userChoice}.`
     }else{
@@ -29,18 +24,32 @@ function playRound(computerChoice, userChoice, playerScore, computerScore){
 function game(){
     let playerScore = 0;
     let computerScore = 0;
-    console.log("Welcome to Rock, Paper, Scissors!");
-    for (let i = 0; i<5; i++){
-        let userChoice = prompt("Would you like to play Rock, Paper, or Scissors?");
-        userChoice = userChoice.toLowerCase();
-        userChoice = capitalize(userChoice);
-        let computerChoice = getComputerChoice();
-        let roundResults = playRound(computerChoice, userChoice, playerScore, computerScore) 
-        playerScore = roundResults.playerScore;
-        computerScore = roundResults.computerScore;
-        console.log(roundResults.result);
-        console.log(`Player score: ${playerScore}    Computer score: ${computerScore}`);
-    } 
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const roundWinner = document.querySelector('.round-winner');
+        const pScore = document.querySelector('.player-score');
+        const cScore = document.querySelector('.computer-score');
+        const winner = document.querySelector('.winner');
+        let buttons = document.querySelectorAll('.buttons');
+    
+        buttons.forEach((button) => {
+            button.addEventListener('click', () => {
+                let userChoice = button.id;
+                let computerChoice = getComputerChoice();
+                let roundResults = playRound(computerChoice, userChoice, playerScore, computerScore) 
+                playerScore = roundResults.playerScore;
+                computerScore = roundResults.computerScore;
+                roundWinner.innerText = roundResults.result;
+                pScore.innerText = `Player score: ${playerScore}`; 
+                cScore.innerText = `Computer score: ${computerScore}`;
+                if (playerScore === 5){
+                    winner.innerText = "Player wins!";
+                } else if (computerScore === 5){
+                    winner.innerText = "Computer wins.";
+                }
+            });
+        });
+    });
 }
 
 game();
